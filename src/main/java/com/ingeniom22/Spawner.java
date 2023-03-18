f48c970b41fd7cf027bfb0a7170656644db92e61
ingeniom22 <jamesmichael0444@gmail.com>
2023-03-18 18:06:31 +0700

    add some documentation

--- src/main/java/com/ingeniom22/Spawner.java
+++ src/main/java/com/ingeniom22/Spawner.java
@@ -29,6 +29,7 @@ public class Spawner implements Listener {
             World world = p.getWorld();
             Location playerLocation = p.getLocation();
 
+            // get initial count of nearby zombie
             int nearbyZombie = 0;
             for (Entity e : world.getEntities()) {
                 if (e instanceof Zombie) {
@@ -40,19 +41,22 @@ public class Spawner implements Listener {
                 }
             }
 
+            // spawn zombie if less than threshold
             if (nearbyZombie < MAX_ZOMBIE) {
-                // Get a random location within 10 blocks of the player's location
+                // Get a random location within 24 blocks of the player's location
                 double x = playerLocation.getX() + (Math.random() * 48) - 24;
                 double y = playerLocation.getY() + 1;
                 double z = playerLocation.getZ() + (Math.random() * 48) - 24;
                 Location spawnLocation = new Location(world, x, y, z);
 
-                // Spawn a zombie at the random location
+                // Spawn a zombie at the random location if block is
+                // spawnable and distance with player
+                // is greater than 16
                 if (Utils.canZombieSpawn(spawnLocation) && playerLocation.distance(spawnLocation) > 16) {
                     world.spawn(spawnLocation, Zombie.class);
                     nearbyZombie++;
                     System.out.println("Spawning zombie nearby " + p.getName() + "at" + x + y + z);
-                } else{
+                } else {
                     continue;
                 }
 
