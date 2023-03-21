755a91bd0a522078e6f7fae4d6d3eb8244378199
ingeniom22 <jamesmichael0444@gmail.com>
2023-03-21 14:50:20 +0700

    add KingdomX integration

--- src/main/java/com/ingeniom22/Utils.java
+++ src/main/java/com/ingeniom22/Utils.java
@@ -2,19 +2,22 @@ package com.ingeniom22;
 
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.kingdoms.constants.land.Land;
 
 public class Utils {
+
     public static boolean canZombieSpawn(Location l) {
         World w = l.getWorld();
         int x = l.getBlockX();
         int y = l.getBlockY();
         int z = l.getBlockZ();
+        Land land = Land.getLand(l);
 
-        return (w.getBlockAt(x, y, z).isEmpty()
+        return (!land.isClaimed()
+                && w.getBlockAt(x, y, z).isEmpty()
                 && w.getBlockAt(x, y + 1, z).isEmpty()
                 && w.getBlockAt(x, y + 2, z).isEmpty()
-                && !w.getBlockAt(x, y -1, z).isEmpty()
-                );
+                && !w.getBlockAt(x, y - 1, z).isEmpty());
 
     }
 }
